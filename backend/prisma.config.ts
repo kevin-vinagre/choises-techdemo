2fb6dc0c229f34f247a7b0d448119a0b6496a23e
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-09-16 18:46:21 -0400

    prisma db pull integration

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,4 +1,12 @@
-import { definePrismaConfig } from "prisma/config";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
-export default definePrismaConfig({
+export default defineConfig({
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    datasource: {
+        url: env("DATABASE_URL"),
+    },
 });
