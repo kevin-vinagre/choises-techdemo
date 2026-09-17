0a2358f927ec6ba2b95cfba26d6fda20f2a38d08
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-09-16 20:04:16 -0400

    prisma update

--- Data/schemas.sql
+++ Data/schemas.sql
@@ -1,22 +1,20 @@
-CREATE SCHEMA IF NOT EXISTS choisestech;
-
 DO $$ BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type t JOIN pg_namespace n ON n.oid = t.typnamespace WHERE t.typname = 'estatus' AND n.nspname = 'choisestech') THEN
-        CREATE TYPE choisestech.estatus AS ENUM ('ACTIVE','INACTIVE');
+    IF NOT EXISTS (SELECT 1 FROM pg_type t JOIN pg_namespace n ON n.oid = t.typnamespace WHERE t.typname = 'estatus' AND n.nspname = 'public') THEN
+        CREATE TYPE estatus AS ENUM ('ACTIVE','INACTIVE');
     END IF;
 END$$;
 
-CREATE TABLE choisestech.episodes (
+CREATE TABLE episodes (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     description TEXT,
     first_chapter_id BIGINT,
     create_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     update_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    status choisestech.estatus NOT NULL DEFAULT 'ACTIVE'
+    status estatus NOT NULL DEFAULT 'ACTIVE'
 );
 
-CREATE TABLE choisestech.chapters (
+CREATE TABLE chapters (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     episode_id BIGINT NOT NULL,
     video_url TEXT NOT NULL,
@@ -27,10 +25,10 @@ CREATE TABLE choisestech.chapters (
 
     CONSTRAINT fk_chapter_episode
         FOREIGN KEY (episode_id)
-        REFERENCES choisestech.episodes(id)
+        REFERENCES episodes(id)
 );
 
-CREATE TABLE choisestech.answers (
+CREATE TABLE answers (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     chapter_id BIGINT NOT NULL,
     intent VARCHAR(100),
@@ -40,14 +38,14 @@ CREATE TABLE choisestech.answers (
 
     CONSTRAINT fk_answer_chapter
         FOREIGN KEY (chapter_id)
-        REFERENCES choisestech.chapters(id),
+        REFERENCES chapters(id),
 
     CONSTRAINT fk_answer_next_chapter
         FOREIGN KEY (next_chapter_id)
-        REFERENCES choisestech.chapters(id)
+        REFERENCES chapters(id)
 );
 
-ALTER TABLE choisestech.episodes
+ALTER TABLE episodes
 ADD CONSTRAINT fk_episode_first_chapter
 FOREIGN KEY (first_chapter_id)
-REFERENCES choisestech.chapters(id);
+REFERENCES chapters(id);
