94d2f289209affd0a21f8eeed110c135580589fd
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-09-17 20:30:23 -0400

    repository adjust

--- backend/src/shared/users/repositories/users.repository.js
+++ backend/src/shared/users/repositories/users.repository.js
@@ -1,23 +1,49 @@
 import prisma from "../../../core/database/prisma.client"
 
+
 /**
- * Procurando usuario por username 
+ * Listagem de usuarios com campos id,username,status
  */
-async function findByUserName(username) {
-    prisma.users.findUnique({
-        where: {
-            username
+async function listUsers() {
+    return prisma.users.findMany({
+        select: {
+            id: true,
+            username: true,
+            status: true
         }
     });
 }
 
+/**
+ * validar por usuario e senha
+ */
+async function validateUser(username, password) {
+    const user = prisma.users.findFirst({
+        where: {
+            username,
+            password
+        }
+    });
+
+    return !!user;
+}
+
+/**
+ * função de criar usuario
+ */
 async function createUser(data) {
     return prisma.users.create({
-        data
+        data,
+        select: {
+            id: true,
+            username: true,
+            status: true
+        }
     });
 }
 
 export {
-    findByUserName,
+    listUsers,
+    validateUser,
     createUser
 }
