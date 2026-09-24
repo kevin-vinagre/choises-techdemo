f898b6aec98eae5edf5f726f8a8dcf6aa409c91a
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-09-23 20:11:25 -0400

    service adjust

--- backend/src/features/signup/signup.service.js
+++ backend/src/features/signup/signup.service.js
@@ -2,18 +2,18 @@ import bcrypt from 'bcryptjs'
 import { validateByEmail, validateByUsername, createUser } from '../../shared/users/repositories/users.repository'
 
 
-function signUpUser(username, email, password) {
+async function signUpUser(username, email, password) {
     if (!username || !email || !password) {
         const error = new Error("Campos nome de usuario, email e senha são obrigatorios");
         error.statusCode = 500;
         throw error;
     }
-    if (validateByUsername(username)) {
+    if (await validateByUsername(username)) {
         const error = new Error("Nome de usuario ja cadastrado");
         error.statusCode = 409;
         throw error;
     }
-    if (validateByEmail(email)) {
+    if (await validateByEmail(email)) {
         const error = new Error("Email ja registrado");
         error.statusCode = 409;
         throw error;
