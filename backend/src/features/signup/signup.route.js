48759d0a45eb72fcf0deaf9f4994a65d06f2025b
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-09-24 19:50:35 -0400

    api adjust

--- backend/src/features/signup/signup.route.js
+++ backend/src/features/signup/signup.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
 import signupController from './signup.controller'
 const router = express.Router()
-router.post('/signup', signupController)
+router.post('/', signupController)
 export default router
